91169cee83b9730dfeea99164a25aa465020abee
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2025-10-15 23:03:53 -0300

    fix migration

--- src/main/resources/db/migration/V2__update_feedback.sql
+++ src/main/resources/db/migration/V2__update_feedback.sql
@@ -1,3 +1,1 @@
 ALTER TABLE tb_feedback ADD CONSTRAINT uk_tb_feedback_student_course UNIQUE (student_id, course_id);
-ALTER TABLE tb_user     ADD CONSTRAINT uk_tb_user_cpf      UNIQUE (cpf);
-ALTER TABLE tb_user     ADD CONSTRAINT uk_tb_user_email    UNIQUE (email);
